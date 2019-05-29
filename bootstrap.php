5a15f46abb3a7fc345af08d33631ec490cc969a3
khenryjoe <joseph.ketterer@gmail.com>
2019-05-29 14:47:08 +0100

    css changes

--- bootstrap.php
+++ bootstrap.php
@@ -1,17 +1,16 @@
 <?php
 
 
-
 require_once './includes/config.php';
 require_once './lang/'.$lang['language'].'.php';
 
 // autoload classes
-function myAutoloader($class){
+function autoloader($class){
   
     require_once './classes/'.$class.'.php';
-    }
-    // Register the function with PHP...
-    spl_autoload_register('myAutoloader');
+}
+
+spl_autoload_register('autoloader');
 
 
 
--- includes/config.php
+++ includes/config.php
@@ -3,10 +3,10 @@
 /* DB variables */
 
 
-$config['DB_HOST'] = 'localhost';
-$config['DB_NAME'] = 'test3';
-$config['DB_USER'] = 'root';
-$config['DB_PASS'] = '';
+$config['DB_HOST'] = 'mysqlsrv.dcs.bbk.ac.uk';
+$config['DB_NAME'] = 'jkette01db';
+$config['DB_USER'] = 'jkette01';
+$config['DB_PASS'] = 'bbkmysql';
 
 /* Set the default timezone ;*/
 date_default_timezone_set('Europe/London');

37bb3cafb2a8819d0f302e45cd36ca1c56351a4a
khenryjoe <joseph.ketterer@gmail.com>
2019-06-05 16:53:50 +0100

    tidying form

--- index.php
+++ index.php
@@ -1,27 +1,22 @@
 <?php 
 require_once './bootstrap.php';
-if(isset($_POST['submit'])) { 
-    $language = htmlentities(trim($_POST['language']));
-    if(( $language == 'fr')||($language == 'en')){ 
-    $cookie_name = "language";
-    $cookie_value = $language;
-    setcookie($cookie_name,  $language, time() + 3600, '/');
-    header("Refresh:0");
+
+// this handles form - which changes the language. Im using a cookie to store the language variable.
+if(isset($_POST['submit'])) {
+    if(isset($_POST['language'])) {
+        $language = htmlentities(trim($_POST['language']));
+        if(( $language == 'fr')||($language == 'en')){
+            $cookie_name = "language";
+            $cookie_value = $language;
+            setcookie($cookie_name,  $language, time() + 3600, '/');
+            header("Refresh:0");
+        }
     }
 }
 
 
 // JKETTE01
 // Building Web Applications using MySQL and PHP
-
-
-
-
-
-
-
-
-
 
 
 if (!isset($_GET['page'])) {
